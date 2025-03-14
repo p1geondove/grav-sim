b100e2afddf8caafe4b0768eb149f1cd4b1dda8c
p1geondove <leo.peil@web.de>
2025-03-14 20:32:58 +0100

    add figure 8 orbit

--- startpos.py
+++ startpos.py
@@ -1,14 +1,23 @@
 from ui_elements import Ball
 import numpy as np
 from random import randint
+from math import pi
 
+SCALE = 12
+VX = 0.3471128135672417 * SCALE
+VY = 0.532726851767674 * SCALE
+SCALE *= SCALE
 ARRANGEMENTS = [
     [
-        Ball(20, np.array([400, 300]), np.array([3, 0])),
-        Ball(20, np.array([500, 400]), np.array([0, 3])),
+        Ball(20, np.array([400, 300]), np.array([ 3, 0])),
+        Ball(20, np.array([500, 400]), np.array([ 0, 3])),
         Ball(20, np.array([400, 500]), np.array([-3, 0])),
-        Ball(20, np.array([300, 400]), np.array([0, -3])),
-    ],
+        Ball(20, np.array([300, 400]), np.array([ 0,-3])),
+    ],[
+        Ball(pi**0.5/pi*SCALE, np.array([400 - SCALE, 400]), np.array([   VX, -VY])),
+        Ball(pi**0.5/pi*SCALE, np.array([400        , 400]), np.array([-2*VX,2*VY])),
+        Ball(pi**0.5/pi*SCALE, np.array([400 + SCALE, 400]), np.array([   VX, -VY])),
+    ]
 ]
 
 def get_random(idx=None):
