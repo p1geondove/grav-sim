08d54b9456a7b89af131df3de11fe42e13da7542
p1geondove <leo.peil@web.de>
2025-03-16 08:46:56 +0100

    add function to get monitor size and store it in const.Var

--- util.py
+++ util.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pygame
+import numpy as np
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
@@ -14,3 +15,8 @@ def resource_path(relative_path):
 
 def set_icon():
     pygame.display.set_icon(pygame.image.load(resource_path('./assets/logo.ico')))
+
+def get_monitor():
+    from const import Var
+    pygame.display.init()
+    Var.monitor_size = np.array(pygame.display.get_desktop_sizes()[0])
